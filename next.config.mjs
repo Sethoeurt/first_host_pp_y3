70a1d1a6a717764a03ea0ce10cd99f256cee6bad
Sethoeurt <sethoeurt22@gmail.com>
2024-12-30 12:50:11 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
+//next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: '2.bp.blogspot.com',
+        },
+        {
+          protocol: 'https',
+          hostname: '2.bp.blogspot.com',
+        },
+        {
+          protocol: 'http',
+          hostname: '*.blogspot.com',
+        },
+        {
+          protocol: 'https',
+          hostname: '*.blogspot.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'tse3.mm.bing.net',
+        },
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig; // Use `export default` for ES modules
